a5c8b07047ede0aec7ab4965b39cb3f59b410c74
Habip Hakkioglu <h.hakkioglu@icloud.com>
2024-07-23 20:24:09 +0200

    Navbar, dropdown menu

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import DashboardLayout from './layout';
+
+const Dashboard = () => {
+  return (
+    <DashboardLayout>
+      <div>
+          Dashboard Page
+      </div>
+    </DashboardLayout>
+      
+  )
+}
+
+export default Dashboard;
